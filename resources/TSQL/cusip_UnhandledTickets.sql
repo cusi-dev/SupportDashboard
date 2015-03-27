c42c38fafed43b71445ca38a845946e822aa79d8
Tom Scrape <tom.scrape@gmail.com>
2015-03-27 07:28:47 -0500

    Updated cusip_UnhandledTickets query to check for lines containing customer self-assigned which creates CC entries in mrAssignees field

--- resources/TSQL/cusip_UnhandledTickets.sql
+++ resources/TSQL/cusip_UnhandledTickets.sql
@@ -1,6 +1,6 @@
 USE [Footprints]
 GO
-/****** Object:  StoredProcedure [dbo].[cusip_UnhandledTickets]    Script Date: 3/25/2015 8:09:37 AM ******/
+/****** Object:  StoredProcedure [dbo].[cusip_UnhandledTickets]    Script Date: 3/27/2015 7:26:49 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -8,14 +8,6 @@ GO
 ALTER PROCEDURE [dbo].[cusip_UnhandledTickets]
 AS
 BEGIN
-
-	--SELECT TOP (20)
-	--	UnhandledTickets
-	--FROM
-	--	[tblCusiMetricsUnhandledTickets]
-	--ORDER BY
-	--	[DateCaptured]
-	--DESC
 
 	SELECT 
 		COUNT(*) AS Tickets 
@@ -27,9 +19,16 @@ BEGIN
 		m.mrid=ma.mrID
 	WHERE 
 		m.mrSTATUS NOT IN ('Escalated__b__u__bTier__b2','Escalated__b__u__bDevelopment','Escalated__b__u__bCBSW__bDevelopment','Assigned','In__bProgress','Closed','Resolved', '_DELETED_', 'Client__bAcceptance', 'Contracted__bWork', 'Development', 'Pending')
-	AND 
-		m.mrASSIGNEES = 'Support'
-	AND 
+	AND --First filter all non-support tickets
+		m.mrASSIGNEES LIKE 'Support%' 
+	AND --Check for Support as the only assignee after stripping CCs (which always come at the end of the assignee string)
+		RTRIM(LEFT(m.mrASSIGNEES,(
+					CASE 
+						WHEN CHARINDEX('cc',m.mrAssignees) > 0 THEN CHARINDEX('cc',m.mrAssignees) - 1
+						ELSE LEN(m.mrAssignees)
+					END
+		))) = 'Support'
+	AND --Empty scheduled call time or the scheduled call time is before tomorrow.
 	(
 			Scheduled__bCall IS NULL
 		OR
