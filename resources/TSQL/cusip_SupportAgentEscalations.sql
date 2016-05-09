1a6d466055d54f72aaf7987b96c0f5af822e0f6a
Tom Scrape <tom.scrape@gmail.com>
2016-05-09 14:35:26 -0500

    Update for display in individual donut widget

--- resources/TSQL/cusip_SupportAgentEscalations.sql
+++ resources/TSQL/cusip_SupportAgentEscalations.sql
@@ -10,21 +10,37 @@ BEGIN
 
 	SET NOCOUNT ON
 
-	SELECT 
-		m.mrID,
-		CAST(m.mrUPDATEDATE AS SMALLDATETIME) [Timestamp],
-		CAST((DATEDIFF(MINUTE,CAST(m.mrUPDATEDATE AS SMALLDATETIME),GETDATE()) / 60) AS VARCHAR) + ':' + RIGHT('00' + RTRIM(CAST((DATEDIFF(MINUTE,CAST(m.mrUPDATEDATE AS SMALLDATETIME),GETDATE()) % 60) AS VARCHAR)),2) Age,
-		RIGHT(i.StatusDisplayName,LEN(i.StatusDisplayName) - 12) [Status]
-	FROM 
-		MASTER4 m
-	INNER JOIN
-		MASTER4_ISSUESTATUS i
-	ON
-		i.IssueStatusName = m.mrSTATUS
-	WHERE
-		m.mrSTATUS IN ('Escalated__b__u__bDevelopment','Escalated__b__u__bCBSW__bDevelopment','Escalated__b__u__bTier__b2')
-	ORDER BY
-		m.mrUPDATEDATE
+	SELECT
+		[Status] 'Escalation',
+		COUNT(mrid) 'Count'
+	FROM (
+		SELECT
+			m.mrID,
+			CAST(m.mrUPDATEDATE AS SMALLDATETIME) [Timestamp],
+			CAST((DATEDIFF(MINUTE,CAST(m.mrUPDATEDATE AS SMALLDATETIME),GETDATE()) / 60) AS VARCHAR) + ':' + RIGHT('00' + RTRIM(CAST((DATEDIFF(MINUTE,CAST(m.mrUPDATEDATE AS SMALLDATETIME),GETDATE()) % 60) AS VARCHAR)),2) Age,
+			CASE
+				WHEN LEFT(i.StatusDisplayName,9) = 'Escalated' THEN RIGHT(i.StatusDisplayName,LEN(i.StatusDisplayName) - 12)
+				WHEN i.StatusDisplayName = 'Pending' AND m.Pending__bSub__ustatus = 'Backlog' THEN 'Pend-Backlog'
+				WHEN i.StatusDisplayName = 'Pending' AND m.Pending__bSub__ustatus = 'Development' THEN 'Pend-Devel'
+				ELSE i.StatusDisplayName
+			END [Status]
+		FROM
+			MASTER4 m
+		INNER JOIN
+			MASTER4_ISSUESTATUS i
+		ON
+			i.IssueStatusName = m.mrSTATUS
+		WHERE
+		(
+			m.mrSTATUS IN ('Escalated__b__u__bDevelopment','Escalated__b__u__bCBSW__bDevelopment','Escalated__b__u__bTier__b2','Escalated__b__u__bMgmt')
+		OR
+			(m.mrSTATUS = 'Pending' AND m.Pending__bSub__ustatus IN ('Backlog','Development'))
+		)
+		AND
+			(mrASSIGNEES LIKE 'Support%' OR mrASSIGNEES LIKE ' Support%')
+	) A
+	GROUP BY
+		[Status]
 
 	SET NOCOUNT OFF
 
