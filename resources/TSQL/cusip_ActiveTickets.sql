ac1f86745d57200924874e6930a13aba5828f306
Tom Scrape <tom.scrape@gmail.com>
2016-05-09 14:19:27 -0500

    Update assignees for cases when Support team is not first in list

--- resources/TSQL/cusip_ActiveTickets.sql
+++ resources/TSQL/cusip_ActiveTickets.sql
@@ -14,7 +14,9 @@ BEGIN
 		MASTER4 m
 	WHERE 
 		m.mrSTATUS NOT IN ('Closed','Resolved', '_DELETED_', 'Client__bAcceptance', 'Contracted__bWork', 'Development', 'Pending','Escalated__b__u__bDevelopment','Escalated__b__u__bCBSW__bDevelopment','Escalated__b__u__bMgmt','Escalated__b__u__bTier__b2')
-		AND m.mrASSIGNEES LIKE 'Support%'
-		AND NOT m.Scheduled__bCall >= DATEADD(d,1,CAST(GETDATE() AS DATE))
+	AND
+		(mrASSIGNEES LIKE 'Support%' OR mrASSIGNEES LIKE ' Support%')
+	AND
+		NOT m.Scheduled__bCall >= DATEADD(d,1,CAST(GETDATE() AS DATE))
 
 END
