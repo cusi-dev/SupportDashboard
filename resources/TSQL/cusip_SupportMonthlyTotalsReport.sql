28ab8147d145549c14ba30959851269000be3fe1
Tom Scrape <tom.scrape@gmail.com>
2017-04-24 13:47:17 -0500

    Include monthly totals for issue reporting
    Add inbound, chat, and email issue handling totals per month for
    specialty reporting

--- resources/TSQL/cusip_SupportMonthlyTotalsReport.sql
+++ resources/TSQL/cusip_SupportMonthlyTotalsReport.sql
@@ -12,72 +12,219 @@ BEGIN
 
 CREATE TABLE #tmpMonthlyNewTickets
 (
-	MonthNumber INT,
-	MonthAbbv NVARCHAR(20),
-	YearNumber INT,
-	TicketCount INT,
-	UMSCount INT,
-	CBSWCount INT,
-	ResponseTime INT,
-	SLA60 INT
+    MonthNumber INT,
+    MonthAbbv NVARCHAR(20),
+    YearNumber INT,
+    TicketCount INT,
+    UMSCount INT,
+    CBSWCount INT,
+    ResponseTime INT,
+    SLA60 INT,
+    InboundTickets INT DEFAULT 0,
+    ChatTickets INT DEFAULT 0,
+    EmailTickets INT DEFAULT 0
 )
 
 INSERT INTO #tmpMonthlyNewTickets
 (
-	MonthNumber,
-	MonthAbbv,
-	YearNumber,
-	TicketCount,
-	UMSCount,
-	CBSWCount
+    MonthNumber,
+    MonthAbbv,
+    YearNumber,
+    TicketCount,
+    UMSCount,
+    CBSWCount
 )
 SELECT
-	DATEPART(MONTH,m.mrSUBMITDATE) tMonthNo,
-	CONVERT(CHAR(3),m.mrSUBMITDATE,0) tMonth,
-	DATEPART(YEAR,m.mrSUBMITDATE) tYear,
-	COUNT(m.mrID) tCount,
-	SUM(
-		CASE
-			WHEN a.Application = 'UMS' THEN 1
-			ELSE 0
-		END
-	),
-	SUM(
-		CASE
-			WHEN a.Application = 'CBSW' THEN 1
-			ELSE 0
-		END
-	)
+    DATEPART(MONTH,m.mrSUBMITDATE) tMonthNo,
+    CONVERT(CHAR(3),m.mrSUBMITDATE,0) tMonth,
+    DATEPART(YEAR,m.mrSUBMITDATE) tYear,
+    COUNT(m.mrID) tCount,
+    SUM(
+        CASE
+            WHEN a.Application = 'UMS' THEN 1
+            ELSE 0
+        END
+    ),
+    SUM(
+        CASE
+            WHEN a.Application = 'CBSW' THEN 1
+            ELSE 0
+        END
+    )
 FROM
-	master4 m
+    master4 m
 INNER JOIN
-	MASTER4_ABDATA a
+    MASTER4_ABDATA a
 ON
-	a.mrID=m.mrID
+    a.mrID=m.mrID
 WHERE
-	m.mrSTATUS NOT IN (
-		'_DELETED_',
-		'_PENDING_SOLUTION_',
-		'_SOLVED_',
-		'Draft__bSolution'
-	)
+    m.mrSTATUS NOT IN (
+        '_DELETED_',
+        '_PENDING_SOLUTION_',
+        '_SOLVED_',
+        'Draft__bSolution'
+    )
 AND
-	(mrASSIGNEES LIKE 'Support%' OR mrASSIGNEES LIKE '% Support%')
+    (mrASSIGNEES LIKE 'Support%' OR mrASSIGNEES LIKE '% Support%')
 GROUP BY
-	DATEPART(YEAR,m.mrSUBMITDATE),DATEPART(MONTH,m.mrSUBMITDATE),CONVERT(CHAR(3),m.mrSUBMITDATE,0)
+    DATEPART(YEAR,m.mrSUBMITDATE),DATEPART(MONTH,m.mrSUBMITDATE),CONVERT(CHAR(3),m.mrSUBMITDATE,0)
 ORDER BY
-	tYear, tMonth
+    tYear, tMonth
+
+UPDATE
+    #tmpMonthlyNewTickets
+SET
+    InboundTickets = ISNULL(A.InboundTickets, 0)
+FROM
+(
+    SELECT
+        COUNT(mrID) InboundTickets,
+        DATEPART(MONTH,mrTIMESTAMP) tMonth,
+        DATEPART(YEAR,mrTIMESTAMP) tYear
+    FROM
+    (
+        SELECT 
+            fh.mrID,
+            MIN(fh.mrTIMESTAMP) mrTIMESTAMP
+        FROM 
+            MASTER4_FIELDHISTORY fh
+        INNER JOIN 
+            MASTER4 m
+        ON 
+            m.mrID = fh.mrID
+        INNER JOIN
+            MASTER4_ABDATA ma
+        ON
+            m.mrID = ma.mrID
+        WHERE 
+            fh.mrFIELDNAME = 'mrStatus'
+        AND
+            fh.mrNEWFIELDVALUE = 'Closed'
+        AND
+            m.First__bContact__bResolution = 'on'
+        AND
+            m.mrSTATUS <> '_DELETED_'
+        AND
+            (m.mrASSIGNEES LIKE 'Support%' OR m.mrASSIGNEES LIKE '% Support%')
+        GROUP BY
+            fh.mrID
+    ) B
+    GROUP BY
+        DATEPART(MONTH,mrTIMESTAMP),
+        DATEPART(YEAR,mrTIMESTAMP)
+) A
+WHERE
+    A.tMonth = #tmpMonthlyNewTickets.MonthNumber
+AND
+    A.tYear = #tmpMonthlyNewTickets.YearNumber
+
+UPDATE
+    #tmpMonthlyNewTickets
+SET
+    ChatTickets = ISNULL(A.ChatTickets, 0)
+FROM
+(
+    SELECT
+        COUNT(mrID) ChatTickets,
+        DATEPART(MONTH,mrTIMESTAMP) tMonth,
+        DATEPART(YEAR,mrTIMESTAMP) tYear
+    FROM
+    (
+        SELECT 
+            fh.mrID,
+            MIN(fh.mrTIMESTAMP) mrTIMESTAMP
+        FROM 
+            MASTER4_FIELDHISTORY fh
+        INNER JOIN 
+            MASTER4 m
+        ON 
+            m.mrID = fh.mrID
+        INNER JOIN
+            MASTER4_ABDATA ma
+        ON
+            m.mrID = ma.mrID
+        WHERE 
+            fh.mrFIELDNAME = 'mrStatus'
+        AND
+            fh.mrNEWFIELDVALUE = 'Closed'
+        AND
+            m.Chat__bResolution = 'on'
+        AND
+            m.mrSTATUS <> '_DELETED_'
+        AND
+            (m.mrASSIGNEES LIKE 'Support%' OR m.mrASSIGNEES LIKE '% Support%')
+        GROUP BY
+            fh.mrID
+    ) B
+    GROUP BY
+        DATEPART(MONTH,mrTIMESTAMP),
+        DATEPART(YEAR,mrTIMESTAMP)
+) A
+WHERE
+    A.tMonth = #tmpMonthlyNewTickets.MonthNumber
+AND
+    A.tYear = #tmpMonthlyNewTickets.YearNumber
+
+UPDATE
+    #tmpMonthlyNewTickets
+SET
+    EmailTickets = ISNULL(A.EmailTickets, 0)
+FROM
+(
+    SELECT
+        COUNT(mrID) EmailTickets,
+        DATEPART(MONTH,mrTIMESTAMP) tMonth,
+        DATEPART(YEAR,mrTIMESTAMP) tYear
+    FROM
+    (
+        SELECT 
+            fh.mrID,
+            MIN(fh.mrTIMESTAMP) mrTIMESTAMP
+        FROM 
+            MASTER4_FIELDHISTORY fh
+        INNER JOIN 
+            MASTER4 m
+        ON 
+            m.mrID = fh.mrID
+        INNER JOIN
+            MASTER4_ABDATA ma
+        ON
+            m.mrID = ma.mrID
+        WHERE 
+            fh.mrFIELDNAME = 'mrStatus'
+        AND
+            fh.mrNEWFIELDVALUE = '_REQUEST_'
+        AND
+            fh.mrOLDFIELDVALUE IS NULL
+        AND
+            m.mrSTATUS <> '_DELETED_'
+        AND
+            (m.mrASSIGNEES LIKE 'Support%' OR m.mrASSIGNEES LIKE '% Support%')
+        GROUP BY
+            fh.mrID
+    ) B
+    GROUP BY
+        DATEPART(MONTH,mrTIMESTAMP),
+        DATEPART(YEAR,mrTIMESTAMP)
+) A
+WHERE
+    A.tMonth = #tmpMonthlyNewTickets.MonthNumber
+AND
+    A.tYear = #tmpMonthlyNewTickets.YearNumber
 
 SELECT
-	MonthAbbv,
-	YearNumber,
-	TicketCount,
-	UMSCount,
-	CBSWCount
+    MonthAbbv,
+    YearNumber,
+    TicketCount,
+    UMSCount,
+    CBSWCount,
+    InboundTickets,
+    ChatTickets,
+    EmailTickets
 FROM
-	#tmpMonthlyNewTickets
+    #tmpMonthlyNewTickets
 ORDER BY
-	YearNumber, MonthNumber
+    YearNumber, MonthNumber
 
 DROP TABLE #tmpMonthlyNewTickets
 
